03aae2702691a8ae877cf984f6ccee4948d6e4f5
Clint Winter <ClintWinter@users.noreply.github.com>
2022-12-20 17:51:12 -0500

    fix zsh config + improve test settings

--- nvim/lua/user/pconfig/test.lua
+++ nvim/lua/user/pconfig/test.lua
@@ -5,6 +5,10 @@ vim.keymap.set('n', '<leader>tl', vim.cmd.TestLast)
 vim.keymap.set('n', '<leader>tg', vim.cmd.TestVisit)
 
 
-vim.cmd("let test#strategy = 'basic'")
-vim.cmd("let test#php#phpunit#executable = 'docker exec givebutter-php-fpm php artisan test'")
-vim.cmd("let test#php#phpunit#options = '--verbose'")
+
+vim.cmd('let test#php#phpunit#executable = \'docker exec givebutter-php-fpm php artisan test\'')
+vim.cmd('let test#php#phpunit#options = \'--verbose\'')
+
+local strategy = 'basic'
+vim.cmd(string.format('let test#strategy = \'%s\'', strategy))
+vim.cmd(string.format('let g:test#%s#start_normal = 1', strategy))
