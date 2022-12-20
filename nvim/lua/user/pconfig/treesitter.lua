b5c738bf5c0d293b9f09ce0cce7015a4ec74db39
Clint Winter <clint@macbook-air.myfiosgateway.com>
2022-12-19 20:36:15 -0500

    clean up

--- nvim/lua/user/pconfig/treesitter.lua
+++ nvim/lua/user/pconfig/treesitter.lua
@@ -6,17 +6,11 @@ end
 
 configs.setup {
     ensure_installed = { "help", "javascript", "typescript", "lua", "php" },
-
     sync_install = false,
-
     auto_install = true,
-
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
     },
-
-    indent = {
-        enable = true,
-    },
+    indent = { enable = true },
 }
